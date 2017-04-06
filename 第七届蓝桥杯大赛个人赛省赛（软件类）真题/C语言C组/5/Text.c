7b9196f4eacddba5f6e38feea32caa52583068fd
wscloli <2549271039@qq.com>
2017-04-06 14:18:11 +0800

    蓝桥

---
+++ 第七届蓝桥杯大赛个人赛省赛（软件类）真题/C语言C组/5/Text.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void swap(int a[],int i,int j){
+    int t=a[i];
+    a[i]=a[j];
+    a[j]=t;
+}
+int partition(int a[],int p,int r){
+    
+}
+int qucksort(int a[],int p,int r){
+    if(p<r){
+        int 
+    }
+}
+
+int main(){
+    int i;
+    int a[]={5,13,6,24,2,8,19,27,6,12,17};
+    int N=12;
+    quacksort(a,0,N-1);
+    for(i=0;i<N;i++)printf("%d",a[i]);
+    printf("\n");
+    return 0;
+    }
