02094db810644ad944c0cd6956c1120bb9c6770e
wscloli <2549271039@qq.com>
2017-07-28 01:00:00 +0800

    study

--- java学习/Wsc/src/com/wsc/extend/Animal.java
+++ java学习/Wsc/src/com/wsc/extend/Animal.java
@@ -1,6 +1,9 @@
 package com.wsc.extend;
 
 public class Animal {
+	final int i=3;//final 常量
+	//final 修饰类，不允许被继承
+	//修饰方法，不允许重写
 	String name;
 	int age;
 	String sex;
--- java学习/Wsc/src/com/wsc/extend/Dog.java
+++ java学习/Wsc/src/com/wsc/extend/Dog.java
@@ -1,7 +1,9 @@
 package com.wsc.extend;
 
 public class Dog extends Animal{
+	
 	void eat(){
 		System.out.println("I can eat cat");
+		System.out.println(i);
 	}
 }
