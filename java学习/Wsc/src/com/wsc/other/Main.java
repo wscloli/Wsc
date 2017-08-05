0b900dd01915c64178b68afc0c168f35ea5ec05d
wscloli <2549271039@qq.com>
2017-08-06 00:36:03 +0800

    study

---
+++ java学习/Wsc/src/com/wsc/other/Main.java
@@ -0,0 +1,14 @@
+package com.wsc.other;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//输入
+		Scanner input=new Scanner(System.in);
+		int a=input.nextInt();
+	}
+
+}
---
+++ java学习/Wsc/src/com/wsc/多态/Rectangle.java
@@ -0,0 +1,19 @@
+package com.wsc.多态;
+
+public abstract class Rectangle extends Shape {
+	int a;
+	int b;
+	@Override
+	int c() {
+		// TODO Auto-generated method stub
+		
+		return 0;
+	}
+
+	@Override
+	int s() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
