e5e9244faf759eacf571dc871473dce8eee22a1c
wscloli <2549271039@qq.com>
2017-08-17 23:19:18 +0800

    study

--- android/Text/app/src/main/java/com/example/wsc/text/MainActivity.java
+++ android/Text/app/src/main/java/com/example/wsc/text/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.wsc.text;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //调试信息  Log.d()
+        Log.d("MainActivity","onCreate execute");
     }
 }
