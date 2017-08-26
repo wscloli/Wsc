7610be629a63794c91267680a1053ce1f816158a
wscloli <2549271039@qq.com>
2017-08-26 23:24:34 +0800

    study

--- android/Text/app/src/main/java/com/example/wsc/text/MainActivity.java
+++ android/Text/app/src/main/java/com/example/wsc/text/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.wsc.text;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,6 +12,25 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     //logt+Tab键
     private static final String TAG = "MainActivity";
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"ADD",Toast.LENGTH_SHORT).show();
+            case R.id.remove_item:
+                Toast.makeText(this,"REMOVE",Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +46,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
     }
 }
