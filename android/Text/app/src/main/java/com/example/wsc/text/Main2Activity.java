c7455d071c1d4c594c7c7047332db3123551015c
wscloli <2549271039@qq.com>
2017-09-16 23:52:25 +0800

    study

--- android/Text/app/src/main/java/com/example/wsc/text/Main2Activity.java
+++ android/Text/app/src/main/java/com/example/wsc/text/Main2Activity.java
@@ -1,5 +1,6 @@
 package com.example.wsc.text;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -12,6 +13,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main2,menu);
+        //menu.add(1,100,1,"菜单一");     //动态添加菜单项
         return true;     //显示菜单
     }
 
@@ -19,8 +21,10 @@ public class Main2Activity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){    //判断点击的是那一个菜单项
             case R.id.Test:
-
-                Toast.makeText(this,"测试1",Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(Main2Activity.this,MainActivity.class);
+                 //活动跳转
+                item.setIntent(intent);
+               // Toast.makeText(this,"测试1",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.Test2:
                 Toast toast=Toast.makeText(this,"测试二",Toast.LENGTH_SHORT);
