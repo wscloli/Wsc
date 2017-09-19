0f0546da9885582bbc351ceb76cfba906471fee2
wscloli <2549271039@qq.com>
2017-09-19 19:02:02 +0800

    study

--- android/Text/app/src/main/java/com/example/wsc/text/ContextMenuActivity.java
+++ android/Text/app/src/main/java/com/example/wsc/text/ContextMenuActivity.java
@@ -8,24 +8,26 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class ContextMenuActivity extends AppCompatActivity {
-
+    private String[] data={"文件一","文件二","文件三","文件四","文件五","文件六"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_context_menu);
-        showListView();
+        //showListView();
+   /* }
+    private void showListView(){*/
+
+        ArrayAdapter<String>adapter=new ArrayAdapter(ContextMenuActivity.this,android.R.layout.simple_list_item_1,data);
+        ListView listView=(ListView)findViewById(R.id.list_View);
+        listView.setAdapter(adapter);
     }
-    private void showListView(){
-        ListView listView=(ListView)findViewById(R.id.listView);
-        ArrayAdapter<String>adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,getData() );
-    }
-    private ArrayList<String>getData(){
+   /* private ArrayList<String>getData(){
     ArrayList<String>list=new ArrayList<String>();
         for (int i=0;i<5;i++) {
-            list.add("文件" + (i + 1));
+            list.add("文件" + (i+1));
         }
         return list;
-        }
+        }*/
     }
 
 
